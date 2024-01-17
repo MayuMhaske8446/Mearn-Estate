cec27b16a77b1a4f9e3417475d32e53fc1074676
MayuMhaske8446 <mayumhaske8446@outlook.com>
2024-01-17 13:10:32 +0530

    Add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoter from "./routes/user.route.js";
 import authRoter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -16,12 +17,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.listen(3000, () => {
     console.log("server is running on port 3000!!!");
 })
 
 app.use("/api/user", userRoter);
 app.use("/api/auth", authRoter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
