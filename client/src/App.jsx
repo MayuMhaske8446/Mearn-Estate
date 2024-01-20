d1fe8e91c8a70c8a2c4a61aa27736c45602969a5
MayuMhaske8446 <mayumhaske8446@outlook.com>
2024-01-20 20:41:34 +0530

    Create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/privateRoute.jsx';
 import CreateListing from './pages/CreateListing.jsx';
 import UpdateListing from './pages/UpdateListing.jsx';
 import Listing from './pages/Listing.jsx';
+import Search from './pages/Search.jsx';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/listing/:listingId" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
